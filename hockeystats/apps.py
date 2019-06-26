e3328db8de663c009949699f82265c759d345a3d
David Freiman <dfreiman@demacmedia.com>
2019-06-26 18:30:22 -0400

    Add hockeystats app manually

---
+++ hockeystats/apps.py
@@ -0,0 +1,7 @@
+"""Exports ObConfig class."""
+
+from django.apps import AppConfig
+
+class HockeyStatsConfig(AppConfig):
+    """Provides config information about the Ob app."""
+    name = 'hockeystats'
--- hockeystats/settings.py
+++ hockeystats/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'hockeystats.apps.HockeyStatsConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
