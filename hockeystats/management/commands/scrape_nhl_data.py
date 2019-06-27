e6d00d45ffce3262cb1bc8bf8491c3cae7160167
David Freiman <dfreiman@demacmedia.com>
2019-06-27 09:28:59 -0400

    documentation

--- hockeystats/management/commands/scrape_nhl_data.py
+++ hockeystats/management/commands/scrape_nhl_data.py
@@ -3,20 +3,32 @@ from selenium import webdriver
 
 
 class Command(BaseCommand):
+    """NHL.com stats scraper"""
+    help = 'pulls in stats from NHL.com and saves them locally'
 
     def __init__(self):
         self._keys = []
         super().__init__()
 
-    help = 'does nothing'
-
     def __build_record(self, elements):
+        """
+        Transforms a series of selenium elements into a dict
+        :param elements: list
+        :return: dict
+        """
         player = {}
         for index, element in enumerate(elements):
             player[self._keys[index]] = element.text
         print(player)
+        return player
 
     def handle(self, *args, **kwargs):
+        """
+        entrypoint for scraper command
+        :param args:
+        :param kwargs:
+        :return:
+        """
         driver = webdriver.Firefox()
         driver.get('http://www.nhl.com/stats/player?report=skaterpercentages&reportType=season&seasonFrom=20182019&seasonTo=20182019&gameType=2&position=D&filter=gamesPlayed,gte,30&sort=shotAttemptsPctg')
         try:
